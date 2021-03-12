ba4b27cb5cd2924699dda010ecdd847b26ca5d72
pletnev-aa <apletnev@list.ru>
2021-03-12 16:19:16 +0400

    add optional argument

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,9 +3,12 @@ import argparse
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Generate diff')
+    parser = argparse.ArgumentParser(prog='gendiff',
+                                     description='Generate diff',
+                                     usage='%(prog)s [-h] [-f FORMAT] first_file second_file')  # noqa: E501
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()  # noqa: F841
 
 
