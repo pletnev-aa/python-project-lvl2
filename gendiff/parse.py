0d61ec3728b5e8674d93985c0f53aadae27855bf
pletnev-aa <apletnev@list.ru>
2021-05-28 00:20:06 +0400

    fixed: opening input files

--- gendiff/parse.py
+++ gendiff/parse.py
@@ -4,9 +4,8 @@ import json
 
 def get_data(file):
     form = file.rsplit('.', 1)[-1]
-    if form == 'json':
-        with open(file) as data_file:
+    with open(file) as data_file:
+        if form == 'json':
             return json.load(data_file)
-    else:
-        with open(file, 'r') as data_file:
-            return yaml.safe_load(data_file.read())
+        else:
+            return yaml.safe_load(data_file)
