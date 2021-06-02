8ad41a5c2fceb955a65261586723686569f09e14
pletnev-aa <apletnev@list.ru>
2021-06-02 22:30:52 +0400

    fixed:

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -11,26 +11,24 @@ TYPES = {
 }
 
 
-def generate_diff(data1, data2, formatter='stylish'):
+def generate_diff(data1, data2, formatter='stylish'):  # noqa: C901
+    def get_diff(input1, input2):
+        diff = {}
+        node = sorted(input1.keys() & input2.keys())
+        added_node = input2.keys() - input1.keys()
+        deleted_node = input1.keys() - input2.keys()
+        for key in node:
+            value1 = input1.get(key)
+            value2 = input2.get(key)
+            if isinstance(value1, dict) and isinstance(value2, dict):
+                diff[key] = {TYPES['nested']: get_diff(value1, value2)}
+            elif value1 == value2:
+                diff[key] = {TYPES['alike']: value1}
+            else:
+                diff[key] = {TYPES['changed']: (value1, value2)}
+        for key in added_node:
+            diff[key] = {TYPES['added']: input2[key]}
+        for key in deleted_node:
+            diff[key] = {TYPES['deleted']: input1[key]}
+        return dict(sorted(diff.items()))
     return format.get_format(get_diff(read(data1), read(data2)), formatter)
-
-
-def get_diff(data1, data2):
-    diff = {}
-    node = sorted(data1.keys() & data2.keys())
-    added_node = data2.keys() - data1.keys()
-    deleted_node = data1.keys() - data2.keys()
-    for key in node:
-        value1 = data1.get(key)
-        value2 = data2.get(key)
-        if isinstance(value1, dict) and isinstance(value2, dict):
-            diff[key] = {TYPES['nested']: get_diff(value1, value2)}
-        elif value1 == value2:
-            diff[key] = {TYPES['alike']: value1}
-        else:
-            diff[key] = {TYPES['changed']: (value1, value2)}
-    for key in added_node:
-        diff[key] = {TYPES['added']: data2[key]}
-    for key in deleted_node:
-        diff[key] = {TYPES['deleted']: data1[key]}
-    return dict(sorted(diff.items()))
--- gendiff/parse.py
+++ gendiff/parse.py
@@ -12,9 +12,9 @@ FILE_FORMATS = {
 
 def read(data):
     data = path.abspath(data)
-    data_form = data.rsplit('.', 1)[-1]
-    if data_form in FILE_FORMATS:
-        return FILE_FORMATS[data_form](open(data))
+    data_format = data.rsplit('.', 1)[-1]
+    if data_format in FILE_FORMATS:
+        return FILE_FORMATS[data_format](open(data))
     else:
         raise NameError(
             'Wrong input data format. Can use only .json or .yml/.yaml'
